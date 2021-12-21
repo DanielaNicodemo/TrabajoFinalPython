838e5ebe608c3a2def4975d4414ce8acde7fa79b
Daniela Antonella Nicodemo Corrente <danielanicodemo@hotmail.com>
2021-12-21 01:51:17 -0300

    termine la clase 20- admin

--- ProyectoCoder/ProyectoCoder/AppCoder/admin.py
+++ ProyectoCoder/ProyectoCoder/AppCoder/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(FormContacto)
+
+admin.site.register(Labiales)
+
+admin.site.register(Cremas)
--- ProyectoCoder/ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/ProyectoCoder/AppCoder/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Curso(models.Model):
-    nombre = models.CharField(max_length=40)
-    camada= models.IntegerField()
 
-class Jugador(models.Model):
-    apellido = models.CharField(max_length=40)   
-    numero= models.IntegerField()
-    esBueno= models.BooleanField()
-     
+class FormContacto(models.Model):
+    nombre = models.CharField(max_length=40)
+    telefono= models.IntegerField()
+    mail = models.CharField(max_length=40)
+    mensaje= models.CharField(max_length=180)
+
+
+
+class Labiales(models.Model):
+    nombre = models.CharField(max_length=40)
+    stock= models.IntegerField()
+    precio= models.IntegerField()
+
+
+class Cremas(models.Model):
+    nombre = models.CharField(max_length=40)
+    stock= models.IntegerField()
+    precio= models.IntegerField()
+
+    def __str__(self):
+
+       return f"CREMA: {self.nombre} STOCK: {self.stock} PRECIO: {self.precio}"    
+    
