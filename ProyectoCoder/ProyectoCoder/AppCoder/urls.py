66973e828c4dbeea566018630075ee5b66f41253
Ezequiel Galardi <eze3769@gmail.com>
2021-12-29 18:26:31 -0300

    corrected root URI

--- ProyectoCoder/ProyectoCoder/AppCoder/urls.py
+++ ProyectoCoder/ProyectoCoder/AppCoder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppCoder import views
 
 urlpatterns = [
-    path('inicio', views.inicio, name= "Inicio"),
+    path('', views.inicio, name= "Inicio"),
     path('makeup', views.makeup, name= "Makeup"),
     path('skincare', views.skincare, name= "Skincare"),
     path('contacto', views.contacto, name= "Contacto"),
