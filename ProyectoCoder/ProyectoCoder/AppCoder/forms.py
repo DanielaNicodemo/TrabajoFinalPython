9ff1d8c2f45dea81e9c7018f73c0eb00a562a1e0
DanielaNicodemo <94941377+DanielaNicodemo@users.noreply.github.com>
2022-01-10 10:38:54 -0300

    Delete ProyectoCoder/ProyectoCoder directory

--- ProyectoCoder/ProyectoCoder/AppCoder/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactoFormulario(forms.Form):
-    nombre= forms.CharField()
-    mail= forms.CharField(required=True)
-    telefono= forms.IntegerField()
-    mensaje= forms.CharField()
